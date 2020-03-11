ee2a68ef6483442e56e34a0c7d82fcc7c08ca9e7
Alexander Terry <alexterry43@gmail.com>
2020-03-11 09:57:39 -0600

    Create models and Service for votes
    Co-Authored-By: jshaworth1961 <jshaworth1961@users.noreply.github.com>

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 import ValueSchema from "../models/Value";
 import ProfileSchema from "../models/Profile";
 import PointSchema from "../models/Point";
+import VisitSchema from "../models/Visit"
 
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
   Profile = mongoose.model("Profile", ProfileSchema);
-  Point = mongoose.model("Point", PointSchema)
+  Point = mongoose.model("Point", PointSchema);
+  visit = mongoose.model("Visit", VisitSchema);
 }
 
 export const dbContext = new DbContext();
