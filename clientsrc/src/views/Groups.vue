3a8ee4495b50bf0fa3b0f977ac6bdf7374eb3d34
Alexander Terry <alexterry43@gmail.com>
2020-03-13 11:57:09 -0600

    remove gutters

--- clientsrc/src/views/Groups.vue
+++ clientsrc/src/views/Groups.vue
@@ -5,8 +5,8 @@
         <h1>Public Groups</h1>
       </div>
       <div class="col-12 col-md-4 card mb-3" v-for="group in groups" :key="group.id">
-        <div class="row no-gutters">
-          <div class="col-md-4 d-flex align-items-center">
+        <div class="row">
+          <div class="col-md-4 d-md-flex align-items-center">
             <img
               :src="group.imageUrl || require('../assets/default-group.png')"
               class="card-img"
