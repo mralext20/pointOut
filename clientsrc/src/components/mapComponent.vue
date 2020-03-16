dd9c61f936d011b918e2de0b79692477dd25b3d7
Alexander Terry <alexterry43@gmail.com>
2020-03-16 14:02:15 -0600

    remove debugger

--- clientsrc/src/components/mapComponent.vue
+++ clientsrc/src/components/mapComponent.vue
@@ -144,7 +144,6 @@ export default {
       this.currentZoom = zoom;
     },
     addPoint(event) {
-      debugger;
       this.showMarker = true;
       this.newPoint.location.coordinates[1] = event.latlng.lat;
       this.newPoint.location.coordinates[0] = event.latlng.lng;
