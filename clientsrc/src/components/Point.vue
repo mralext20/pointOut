7ed7f931956a240cbde9bfc3d0d1496248e22640
Alexander Terry <alexterry43@gmail.com>
2020-03-16 12:08:14 -0600

    remove extranius image

--- clientsrc/src/components/Point.vue
+++ clientsrc/src/components/Point.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="col-12 card col-md-4">
-    <img class="card-img-top" data-src="holder.js/100x180/?text=Image cap" alt="Card image cap" />
     <div class="card-body">
       <h4 class="card-title">{{pointData.title}}</h4>
       <p class="card-text">{{pointData.description}}</p>
